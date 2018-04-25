27337446dd91f6c8e350eb047f6413ccfd0c81e6
hyunsang.choi <ericchoi426@gmail.com>
2018-04-25 16:04:34 +0900

    Directory & File Management API 추가

---
+++ CDirectoryFileHelper/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDirectoryFileHelper
+{
+    class CDirectoryFileHeper
+    {
+        // Constructor
+        CDirectoryFileHeper()
+        {
+
+        }
+        #region [ GetMatchedFiles] : 주어진 Directory에 하위 모든 폴더에서 주어진 이름을 가진 파일을 찾아 경로를 리턴해줌
+        public static int GetMatchedFiles(string path, string file_name, out string[] result)
+        {
+            // find all files which is matched given file_name
+            result = Directory.GetFiles(path, file_name, SearchOption.AllDirectories);
+            return result.Length;
+        }
+        #endregion
+    }
+
+    class CDirectoryFilesAPI
+    {
+        static void Main(string[] args)
+        {
+            #region [ 사용법 ] : GetMatchedFiles
+            string[] result;
+            int num = CDirectoryFileHeper.GetMatchedFiles(@"D:\imdb", "clrpick.tcl", out result);
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
