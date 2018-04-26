d8e0312166e9764237c84edb6bb749c1db66743a
hyunsang.choi <ericchoi426@gmail.com>
2018-04-26 10:08:37 +0900

    update file/directory management

--- CDirectoryFileHelper/Program.cs
+++ CDirectoryFileHelper/Program.cs
@@ -31,16 +31,34 @@ namespace CDirectoryFileHelper
 
             byte[] buffer = new byte[BUF_SIZE];
             int nFReadLen;
-
-            FileStream fs_in = new FileStream("./INPUT/" + file_name, FileMode.Open, FileAccess.Read);
-            FileStream fs_out = new FileStream("./OUTPUT/" + file_name, FileMode.Create, FileAccess.Write);
-
-            while ((nFReadLen = fs_in.Read(buffer, 0, BUF_SIZE)) > 0)
+            using (FileStream fs_in = new FileStream("./INPUT/" + file_name, FileMode.Open, FileAccess.Read),
+                 fs_out = new FileStream("./OUTPUT/" + file_name, FileMode.Create, FileAccess.Write))
             {
-                fs_out.Write(buffer, 0, nFReadLen);
+                while ((nFReadLen = fs_in.Read(buffer, 0, BUF_SIZE)) > 0)
+                {
+                    fs_out.Write(buffer, 0, nFReadLen);
+                }
             }
-            fs_in.Close();
-            fs_out.Close();
+        }
+        #endregion
+        #region [ StreamReader ]
+        public static bool ReadFile(string file_name, ref List<string> result)
+        {
+            if (File.Exists(file_name))
+            {
+                // using문을 사용하면 Diposal를 자동 처리 즉 file close를 알아서 처리해줌
+                using (StreamReader reader = new StreamReader(file_name))
+                {
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                            return true;
+                        result.Add(line);
+                    }
+                }
+            }
+            return false;
         }
         #endregion
     }
@@ -59,6 +77,18 @@ namespace CDirectoryFileHelper
             #endregion
             #region[ CopyFile 예제 ]
             CDirectoryFileHeper.CopyFile("test.exe");
+            #endregion
+            #region[ readFile ]
+            string readFileName = @"..\..\data\program.txt";
+            List<string> datalist = new List<string>();
+            bool success = CDirectoryFileHeper.ReadFile(readFileName, ref datalist);
+            if (success)
+            {
+                foreach (string line in datalist)
+                {
+                    Console.WriteLine(line);
+                }
+            }
             #endregion
 
             Console.ReadKey();
