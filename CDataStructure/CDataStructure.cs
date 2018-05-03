9404c6aeb0bc633ffbcae4d513f4386b5ef578dc
hyunsang.choi <ericchoi426@gmail.com>
2018-05-03 14:16:16 +0900

    add string handle API and its examples

--- CDataStructure/CDataStructure.cs
+++ CDataStructure/CDataStructure.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CDataStructure
@@ -109,6 +110,100 @@ namespace CDataStructure
             }
         }
         #endregion
+        #region[ string : formatting ]
+        public static void PrintFormattedString()
+        {
+            Int32 positiveNum = 123456;
+            Int32 negativeNum = -123456;
+            Single realNumber32 = 123456.789F;
+            Double realNumber64 = 123456.789012345;
+            String text = "String.Format example";
+
+            Console.WriteLine("서식이 지정되지 않은 그대로의 값: ");
+            Console.WriteLine("  positiveNum : {0}", positiveNum);
+            Console.WriteLine("  negativeNum : {0}", negativeNum);
+            Console.WriteLine("  realNumber32: {0}", realNumber32);
+            Console.WriteLine("  realNumber64: {0}", realNumber64);
+            Console.WriteLine("  text        : {0}", text);
+
+            Console.WriteLine("통화 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:c}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:C}", negativeNum);
+            Console.WriteLine("  realNumber32: {0:c}", realNumber32);
+            Console.WriteLine("  realNumber64: {0:C}", realNumber64);
+            Console.WriteLine("  text        : {0:C}", text);
+
+            Console.WriteLine("10진수 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:d}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:D}", negativeNum);
+            Console.WriteLine("  realNumber32: -");
+            Console.WriteLine("  realNumber64: -");
+            Console.WriteLine("  text        : {0:D}", text);
+
+            Console.WriteLine("고정 소숫점 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:f}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:F}", negativeNum);
+            Console.WriteLine("  realNumber32: {0:f}", realNumber32);
+            Console.WriteLine("  realNumber64: {0:F}", realNumber64);
+            Console.WriteLine("  text        : {0:F}", text);
+
+            Console.WriteLine("일반 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:g}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:G}", negativeNum);
+            Console.WriteLine("  realNumber32: {0:g}", realNumber32);
+            Console.WriteLine("  realNumber64: {0:G}", realNumber64);
+            Console.WriteLine("  text        : {0:G}", text);
+
+            Console.WriteLine("숫자 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:n}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:N}", negativeNum);
+            Console.WriteLine("  realNumber32: {0:n}", realNumber32);
+            Console.WriteLine("  realNumber64: {0:N}", realNumber64);
+            Console.WriteLine("  text        : {0:N}", text);
+
+            Console.WriteLine("백분율 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:p}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:P}", negativeNum);
+            Console.WriteLine("  realNumber32: {0:p}", realNumber32);
+            Console.WriteLine("  realNumber64: {0:P}", realNumber64);
+            Console.WriteLine("  text        : {0:P}", text);
+
+            Console.WriteLine("라운드트립 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : -");
+            Console.WriteLine("  negativeNum : -");
+            Console.WriteLine("  realNumber32: {0:r}", realNumber32);
+            Console.WriteLine("  realNumber64: {0:R}", realNumber64);
+            Console.WriteLine("  text        : {0:R}", text);
+
+            Console.WriteLine("16진수 서식이 지정된 값: ");
+            Console.WriteLine("  positiveNum : {0:x}", positiveNum);
+            Console.WriteLine("  negativeNum : {0:X}", negativeNum);
+            Console.WriteLine("  realNumber32: -");
+            Console.WriteLine("  realNumber64: -");
+            Console.WriteLine("  text        : {0:X}", text);
+        }
+        #endregion
+
+        #region[ string : between ]
+        public static String betweenStrings(String text, String start, String end)
+        {
+            int p1 = text.IndexOf(start) + start.Length;
+            int p2 = text.IndexOf(end, p1);
+            if (end == "") return (text.Substring(p1));
+            else
+            {
+                return text.Substring(p1, p2 - p1);
+            }
+        }
+        #endregion
+
+        #region [ string : 정규식 숫자 추출 ]
+        public static string GetDecimal(string input,string replace)
+        {
+            string strNum = Regex.Replace(input, @"\D", replace);
+            return strNum;
+        }
+        #endregion
     }
 
     class CDataStructure
@@ -117,7 +212,34 @@ namespace CDataStructure
         {
             CDSHelper.HandleStringExample1();
             CDSHelper.HandleStringSplit();
-            
+            CDSHelper.PrintFormattedString();
+
+            #region [ example of between ]
+            {
+                String text = "One=1,Two=2,ThreeFour=34";
+
+                Console.WriteLine(CDSHelper.betweenStrings(text, "One=", ",")); // 1
+                Console.WriteLine(CDSHelper.betweenStrings(text, "Two=", ",")); // 2
+                Console.WriteLine(CDSHelper.betweenStrings(text, "ThreeFour=", "")); // 34
+            }
+            #endregion
+
+            #region [ example of GetDecimal ]
+            {
+                string strDel = CDSHelper.GetDecimal("123adkdi67dkdkdkdk7d88888a", " ");
+                //string[] words = strDel.Split(' ');
+                var words = strDel.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("input words :{0} and # of splited words:{1}",strDel,words.Length);
+
+                foreach (string word in words)
+                {
+                    Console.Write("{0} ",word);
+                }
+                Console.WriteLine();
+                
+            }
+            #endregion
+
             // Keep the console window open in debug mode
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
